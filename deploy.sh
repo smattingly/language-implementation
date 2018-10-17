d1b4865f7d864180bbc4bb5339bee891f4b1ba0c
Steve Mattingly <mattinglys@dewv.edu>
2018-10-17 20:10:57 +0000

    Fix automated deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
 # Deploy obfuscated code from ./dist, via GitHub Pages
-javascript-obfuscator . -o ./dist
+javascript-obfuscator ./scripts -o ./dist
 cp ./index.html ./dist
+git subtree push --prefix dist origin gh-pages
